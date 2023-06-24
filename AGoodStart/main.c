56840fa3e63a7068760b5340250989a608789d75
Ricky Memije <rickymemije@gmail.com>
2023-06-23 20:28:12 -0600

    Initial Commit

---
+++ AGoodStart/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  AGoodStart
+//
+//  Created by Ricky Memije on 6/23/23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
