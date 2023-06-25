b94e8f614fb3e6821ffe6b0dec2e11c9e8cea458
Ricky Memije <rickymemije@gmail.com>
2023-06-24 22:50:19 -0600

    Practice on comments, variable, float types, and prints

--- AGoodStart/main.c
+++ AGoodStart/main.c
@@ -8,7 +8,40 @@
 #include <stdio.h>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+    //MARK: Comment And printf() Examples
+    
+    // Print the beginning of the novel -- example of a single-line comment
+    printf("It was the best of times.\n"); // example of print function
+    printf("It was the worst of times. \n");
+    /*Is that actually any good? Maybe it needs a rewrite. */   // example of a multi-line comment
+    
+    //MARK: Variable Decleration Examples
+    
+    // Decared variable named 'weight' of type float
+    float weight;
+    
+    // Assigning a value to weight
+    weight = 14.2;
+    
+    /* Printing to the console the value of 'weight'. %f is used as a placeholder for float variables. \n is an escape sequence that stands for a newline character. */
+    printf("the value of weight is %f.\n", weight);
+    
+    // Calculating the total price by multiplying the weight by 1.99 and assigning it to a new variable named totalPrice
+    // the multiplied by operator is '*'
+    float totalPrice = 1.9 * weight;
+    
+    //Printing to the console the value of 'totalPrice'.
+    printf("The total price is %f.\n", totalPrice);
+    
+    // Practice section
+    
+    float stateTax = .0087;
+    float hamburgerPrice = 2.99;
+    float friesPrice = 1.50;
+    
+    float mealTotalPrice = ((hamburgerPrice + friesPrice) * stateTax) + hamburgerPrice + friesPrice;
+    
+    printf("The price of a hamburger is %f.\nThe price of fries is %f.\nThe meal total is %f.\n", hamburgerPrice, friesPrice, mealTotalPrice);
+    
     return 0;
 }
